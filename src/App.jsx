fffce719b53c49148a59a910b6d4e1fa0acbe15d
zarif <zarif@icloud.com>
2023-11-01 12:24:27 +0500

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,17 @@ import React from "react";
 import Header from "./pages/Header";
 import Main from "./pages/Main";
 import Footer from "./pages/Footer";
+import ReactGA from 'react-ga'
+
+const TRACKING_ID = "G-BQ6CHGCNWK"; 
+ReactGA.initialize(TRACKING_ID);
+ReactGA.pageview(window.location.pathname);
+
+// Отслеживание события
+ReactGA.event({
+  category: 'User',
+  action: 'Logged In',
+});
 
 function App() {
   
