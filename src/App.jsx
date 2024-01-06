20fb173297632fff2e040be552e48fdefccd2df9
zarif <zarif@icloud.com>
2024-01-06 14:48:32 +0500

    test commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,28 +2,31 @@ import React from "react";
 import Header from "./pages/Header";
 import Main from "./pages/Main";
 import Footer from "./pages/Footer";
-import ReactGA from 'react-ga'
+import ReactGA from "react-ga";
+import Snowfall from "react-snowfall";
 
-const TRACKING_ID = "G-BQ6CHGCNWK"; 
+const TRACKING_ID = "G-BQ6CHGCNWK";
 ReactGA.initialize(TRACKING_ID);
 ReactGA.pageview(window.location.pathname);
 
 // Отслеживание события
 ReactGA.event({
-  category: 'User',
-  action: 'Logged In',
+  category: "User",
+  action: "Logged In",
 });
 
 function App() {
-  
-
-  
   return (
     <div className="App">
       <Header />
-      <Main/>
-      <Footer/>
-     
+      <Main />
+      <Footer />
+      <Snowfall snowflakeCount={400}
+      
+      />
+
+      
+
     </div>
   );
 }
